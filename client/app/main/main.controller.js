b7f970978a06e624a0f1f62cd025ad80e5bdd259
George <levyianboss@gmail.com>
2015-10-11 03:27:57 +0000

    added read me

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,21 +2,21 @@
 
 angular.module('workspaceApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
-
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
-
-    $scope.addThing = function() {
-      if($scope.newThing === '') {
-        return;
-      }
-      $http.post('/api/things', { name: $scope.newThing });
-      $scope.newThing = '';
-    };
-
-    $scope.deleteThing = function(thing) {
-      $http.delete('/api/things/' + thing._id);
-    };
+    $scope.features = [
+        {
+          img: "glyphicon glyphicon-globe",
+          title: "Works Everywhere",
+          description: "Traditional desktop computers now represent only 30% of Internet traffic. Your poll must work on the tablets, smart phones, netbooks and notebooks that your visitors are using. Our responsive designs do just that."
+        },
+        {
+          img: "glyphicon glyphicon-flash",
+          title: "Live Results",
+          description: "Live graphs show your poll results immediately in an easy to understand format. One graph will not provide the whole picture, that's why we provide multiple graph types to better describe your results."
+        },
+        {
+          img: "",
+          title: "Social Integration",
+          description: "Free integrated facebook or traditional comments allow your poll voters to provide immediate feedback and discuss results. Social share buttons encourage your poll voters to help spread the word."
+        }
+      ];
   });
